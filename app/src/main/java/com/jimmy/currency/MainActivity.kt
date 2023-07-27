745cd41b6483a88645b7c8347223efd6825b32fe
Jimmy-Ogoul <mgamal@ogoul.com>
2023-07-27 19:19:40 +0300

    Initial commit

---
+++ app/src/main/java/com/jimmy/currency/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jimmy.currency
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val apiKey = "1709d95d70a1074fdd006477f196dbad"
+    }
+}
